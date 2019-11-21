e42799e744e0d83cba02a9bbc70a3fbc350aa7db
Sebastian Hamza <sebby_99@yahoo.com>
2019-11-21 16:57:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+board = ["-","-","-",
+         "-","-","-",
+         "-","-","-",]
+
+def display_board():
+  print(board[0]+ "|" + board[1]+ "|" + board [2])
+  print(board[3]+ "|" + board[4]+ "|" + board [5])
+  print(board[6]+ "|" + board[7]+ "|" + board [8])
+
+def play_game():
+  display_board()
+
+play_game()
+
