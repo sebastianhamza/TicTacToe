da29f23caab992becb994e4a4d40a79972647af5
Sebastian Hamza <>
2021-04-14 18:18:46 +0000

    Deleted function

--- main.py
+++ main.py
@@ -7,8 +7,19 @@ def display_board():
   print(board[3]+ "|" + board[4]+ "|" + board [5])
   print(board[6]+ "|" + board[7]+ "|" + board [8])
 
+def handle_turn():
+  position = input("Choose pos from 1 to 9: ")
+  position = int(position) - 1
+
+  board[position] = "X";
+  display_board();
+
 def play_game():
   display_board()
+  
+
+
 
 play_game()
-
+ 
+ 
